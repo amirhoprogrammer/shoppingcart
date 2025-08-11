f08d73f247aa28bd616acaa93cbad5fa61757e47
amihoprogrammer <amirhanegareshi@gmail.com>
2025-08-11 16:31:28 +0330

    add react router and make home and header.jsx

---
+++ src/Page/Home.jsx
@@ -0,0 +1,9 @@
+import Header from "../feathers/Header";
+function Home() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+export default Home;
---
+++ src/feathers/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <div>
+      <h1>Redux Shopping Cart</h1>
+      <button>
+        <img src="/images/bag-icon.svg" alt="bag-icon" />
+      </button>
+      {/*<div title="Cart" class="cart_icon">
+        <img src="/images/bag-icon.svg" alt="bag-icon" />
+        <span class="badge">0</span>
+  </div>*/}
+    </div>
+  );
+}
+export default Header;
