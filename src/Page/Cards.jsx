5fccfadf64f7d105bb279b11370d526c2f60df8d
amihoprogrammer <amirhanegareshi@gmail.com>
2025-08-12 16:08:00 +0330

    add option for Cards

--- src/Page/Cards.jsx
+++ src/Page/Cards.jsx
@@ -1,6 +1,79 @@
 import Card from "../feathers/Card";
 function Cards() {
-  const option = [{}, {}];
+  const option = [
+    {
+      id: 1,
+      src: "../image/80f9dca34c8f538f1b93eda1b3e6820a7047ba45_1754252368.jpg",
+      name: "کنسول بازی سونی مدل PlayStation 5 Slim نسخه دیجیتال",
+      cost: 43699000,
+    },
+    {
+      id: 2,
+      src: "../image/4f3b55a839b6826adf122d33d98996f96a7ad9fa_1731229811.jpg",
+      name: "عینک واقعیت مجازی مدل Meta Quest 3S 128GB",
+      cost: 318000000,
+    },
+    {
+      id: 3,
+      src: "../image/8812a5f0a6928d46a3db8611a5d2514198d4d951_1754241384.jpg",
+      name: "کنسول بازی هیبریدی نینتندو مدل Switch 2",
+      cost: 53837260,
+    },
+    {
+      id: 4,
+      src: "../image/402588acee067cc258d8c1bed33b65c717a8ea83_1699112037.jpg",
+      name: "دسته بازی پلی استیشن 5 سونی مدل DualSense سری CFI-ZCT1W",
+      cost: 5986700,
+    },
+    {
+      id: 5,
+      src: "../image/aa19eeadee5d1675a5ce62cd67f5833ca103d5a3_1732890954.jpg",
+      name: "فرمان بازی لاجی مدل G923 مخصوص PS4 / PS5",
+      cost: 38000000,
+    },
+    {
+      id: 6,
+      src: "../image/5322e15f5faeae818dde263f54aecf62472302ae_1699970562.jpg",
+      name: "گیم پد دسته بازی پلی استیشن 5 سونی مدل پورتال",
+      cost: 21300000,
+    },
+    {
+      id: 7,
+      src: "../image/e9abfe10e493e693fc2573b5799c3e5bce96685d_1754836062.jpg",
+      name: "کنسول بازی سونی مدل Playstation 4 Slim نسخه دیسک‌خور",
+      cost: 41797970,
+    },
+    {
+      id: 8,
+      src: "../image/c0decbd4b80d6e8e71ee157776df348e9976440a_1754244114.jpg",
+      name: "مجموعه کنسول بازی مایکروسافت مدل Xbox Series S نسخه دیجیتال",
+      cost: 39600000,
+    },
+    {
+      id: 9,
+      src: "../image/48402685e5796b94e8e90aa5700b61f29d4d5982_1609059384.jpg",
+      name: "دسته بازی ایکس باکس مایکروسافت مدل Series S-X",
+      cost: 5400750,
+    },
+    {
+      id: 10,
+      src: "../image/f7b3c422923dba53b30692eb2878a3939752ee1b_1753807608.jpg",
+      name: "بازی DEATH STRANDING 2 : ON THE BEACH مخصوص PS5",
+      cost: 6038590,
+    },
+    {
+      id: 11,
+      src: "../image/5ae48d41daa5984c0ddcce7a09d42e28637c7d14_1720449723.jpg",
+      name: "کنسول بازی قابل حمل گرین لاین مدل GP pro plus GNGPPROGAMGY",
+      cost: 21300000,
+    },
+    {
+      id: 12,
+      src: "../image/d5810763ce96211b029ce4035738bc9ec5170d72_1731771406.jpg",
+      name: "کنسول بازی قابل حمل نینتندو مدل Switch OLED Neon Blue and Neon Red Joy-Con",
+      cost: 27208150,
+    },
+  ];
   return (
     <div className="p-2">
       <Card />
--- src/feathers/Card.jsx
+++ src/feathers/Card.jsx
@@ -1,9 +1,9 @@
 function Card() {
-  const imgUrl = "../image/prod1.png";
+  const imgUrl = "./src/image/402588acee067cc258d8c1bed33b65c717a8ea83_1699112037.jpg";
   const option = {};
   return (
     <div className="shadow-2xl rounded-2xl flex-col p-2">
-      <img src={imgUrl} alt="" />
+      <img src={imgUrl} alt="image" className="size-20" />
       <h3>boAt Airdopes 131</h3>
       <h3>₹ 1,099</h3>
       <button style={{ backgroundColor: "rgb(148, 29, 2)" }}>Add to Cart</button>
